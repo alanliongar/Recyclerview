f484ab82080d655f1f7d92aec19023ba6d4be663
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-04-23 08:13:26 -0300

    Estudando_Parte_1

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //
+        //criação dataclass - ícone, nome e telefone - é uma classe
+        //criação da lista - vários objetos da classe
+        //criar um adapter
+        //set adapter
+        //LinearLayout manager
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+
+
     }
 }
+
+val contacts = listOf(
+    Contact("Alan", "1194327532874", R.drawable.gato),
+    Contact("Gatin", "1194327532874", R.drawable.gato),
+    Contact("Cachorro", "1194327532874", R.drawable.gato),
+    Contact("Macaco", "1194327532874", R.drawable.gato),
+    Contact("Alan", "1194327532874", R.drawable.gato),
+    Contact("Gatin", "1194327532874", R.drawable.gato),
+    Contact("Cachorro", "1194327532874", R.drawable.gato),
+    Contact("Macaco", "1194327532874", R.drawable.gato),
+    Contact("Alan", "1194327532874", R.drawable.gato),
+    Contact("Gatin", "1194327532874", R.drawable.gato),
+    Contact("Cachorro", "1194327532874", R.drawable.gato),
+    Contact("Macaco", "1194327532874", R.drawable.gato),
+    Contact("Alan", "1194327532874", R.drawable.gato),
+    Contact("Gatin", "1194327532874", R.drawable.gato),
+    Contact("Cachorro", "1194327532874", R.drawable.gato),
+    Contact("Macaco", "1194327532874", R.drawable.gato),
+    Contact("Alan", "1194327532874", R.drawable.gato),
+    Contact("Gatin", "1194327532874", R.drawable.gato),
+    Contact("Cachorro", "1194327532874", R.drawable.gato),
+    Contact("Macaco", "1194327532874", R.drawable.gato)
+)
